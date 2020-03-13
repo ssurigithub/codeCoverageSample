38536d224fb18a5802c53359158b1779978a1d36
Saravanan Suriyanarayanan <thalaatorchid@gmail.com>
2020-03-12 23:24:01 -0700

    changed run-tests.sh file - removing sudo

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo dotnet test --logger 'trx;LogFileName=TestResults.trx' --logger 'xunit;LogFileName=TestResults.xml' --results-directory ./calculatorLibrary.Tests/BuildReports/UnitTests  /p:CollectCoverage=true /p:CoverletOutput=BuildReports/Coverage/ /p:CoverletOutputFormat=cobertura /p:Exclude='[xunit.*]*'
+ dotnet test --logger 'trx;LogFileName=TestResults.trx' --logger 'xunit;LogFileName=TestResults.xml' --results-directory ./calculatorLibrary.Tests/BuildReports/UnitTests  /p:CollectCoverage=true /p:CoverletOutput=BuildReports/Coverage/ /p:CoverletOutputFormat=cobertura /p:Exclude='[xunit.*]*'
 echo 'generating reports'
-sudo ./tools/reportgenerator -reports:./calculatorLibrary.Tests/BuildReports/Coverage/coverage.cobertura.xml -targetdir:./calculatorLibrary.Tests/BuildReports/Coverage -reporttypes:"HTML;HTMLSummary"
+ ./tools/reportgenerator -reports:./calculatorLibrary.Tests/BuildReports/Coverage/coverage.cobertura.xml -targetdir:./calculatorLibrary.Tests/BuildReports/Coverage -reporttypes:"HTML;HTMLSummary"
 
                  
                  
